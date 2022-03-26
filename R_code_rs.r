1329166d689e63fc316235c973810d316a1223f0
Carlopelizzola <102289061+Carlopelizzola@users.noreply.github.com>
2022-03-26 17:34:06 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,12 @@
 # questo è primo script che useremo a lezione 
 
+# install.packages("raster")
 library(raster)
+
+# settaggio cartella di lavoro 
+setwd("C:/lab/")
+
+# importazione dati 
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
