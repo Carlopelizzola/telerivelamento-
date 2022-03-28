401fe370bf45d1fc444dc8f77d26ad54c2589009
Carlopelizzola <102289061+Carlopelizzola@users.noreply.github.com>
2022-03-28 18:05:26 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -15,5 +15,28 @@ plot(l2011)
 
 # cambiamento di leggenda 
 cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
-
 plot(l2011, col=cl)
+
+# lensat ETM+
+# b1 = blue
+# b2 = verde
+# b3 = rosso 
+# b4 = infrarosso vicino 
+
+# plot della banda del blu B1_sre
+plot(l2011$B1_sre)
+#oppure
+plot(l2011[[1]])
+
+# cambiamento leggenda 
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011$B1_sre, col=cl)
+
+# plot b1 from dark blue to blue to light blue
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(l2011$B1_sre, col=clb)
+
+# esportazione immagini in pdf nella cartella lab 
+pdf("banda1.pdf")
+plot(l2011$B1_sre, col=clb)
+dev.off()
