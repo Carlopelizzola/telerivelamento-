6fb8d76ef2594c5db33daa8671dcea33652b87da
Carlopelizzola <102289061+Carlopelizzola@users.noreply.github.com>
2022-08-19 22:58:25 +0200

    Update R_code_exam_Nigeria.r

--- R_code_exam_Nigeria.r
+++ R_code_exam_Nigeria.r
@@ -16,14 +16,15 @@
 library(raster) 
 library(RStoolbox) #per vissualizzare le immagini e calcoli 
 library(ggplot2) #per i plot ggplot 
-library(patchtwork) #per creare multiframe con ggplot 
+library(patchwork) #per creare multiframe con ggplot 
 library(viridis) 
 
 # settaggio della cartella di lavoro: 
 setwd("C:/lab/Nigeria_exam")
 
-#
-   C:\lab\Nigeria_exam                                                        ##### IMPORTAZIONE DEI DATI #####
+
+
+                                                              ##### IMPORTAZIONE DEI DATI #####
                                       
                                             ##Importazione dei dati della immagine satellitare del 26/01/2022##
 #CREAZIONE LISTA 
@@ -48,7 +49,42 @@ plot(n2022)
 
 #immagine a colori naturali: 
 #faccio un plot con la banda R nella componente R, banda G nella componente G e banda B in B per osservare l'immagine con i colori naturali 
-n2022_vis <- ggRGB(n2022, 4, 3, 2)
+n2022_vis <- ggRGB(n2022, 4, 3, 2, stretch="lin") + ggtitle("riserva Akure ofusu 2022")
 
 #Nir in R, red in G, green in B. 
-g1_2022 <- ggRGB(n2022, 5, 4, 3)
+g1_2022 <- ggRGB(n2022, 5, 4, 3, stretch="lin") + ggtitle("ggplot 2022") 
+#in questo plot è evidente la vegetazione che appare rossa e il suolo nudo. 
+
+#2014 
+# CREAZIONE LISTA 
+# Avendo scaricato le 7 bande separatamente le devo importare creando una lista con la funzione list.files:
+list_2015 <- list.files(pattern="LC08_L2SP") # quindi creo una lista contenete le bande e la associo a list_2022  
+
+#IMPORTAZIONE
+#una volta creata la lista, applico tramite la funzione lapply la funzione raster a tutta la lista creata per importare tutto.
+import_2015 <- lapply(list_2015, raster)
+
+#CREAZIONE DI UN BLOCCO COMUNE CON TUTTI DATI IMPORTATI: 
+#una volta importato tutte le 7 bande posso creare un blocco comune a tutti i dati importati tramite la funzione stack: 
+Nigeria_2015 <- stack(import_2015)
+
+# Ricampionamento 
+#visto che l'immagine pesa troppo la ricampiono con funzione agregate 
+n2015 <- aggregate(Nigeria_2015, fact=10)
+
+#PLOT 
+#plot normale per vedere le bande 
+plot(n2015)
+
+#immagine a colori naturali: 
+#faccio un plot con la banda R nella componente R, banda G nella componente G e banda B in B per osservare l'immagine con i colori naturali 
+n2015_vis <- ggRGB(n2015, 4, 3, 2, stretch="lin") + ggtitle("riserva Akure ofusu 2015")
+
+#Nir in R, red in G, green in B. 
+g1_2015 <- ggRGB(n2015, 5, 4, 3, stretch="lin") + ggtitle("ggplot 2014") 
+#in questo plot è evidente la vegetazione che appare rossa e il suolo nudo. 
+
+# metto a confronto il plot del 2015 e quello del 2021 
+n2015_vis + n2022_vis
+#
+g1_2015 + g1_2022 
