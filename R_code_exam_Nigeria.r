37529d89ed9bade68dec076c61d60020ecb06c86
Carlopelizzola <102289061+Carlopelizzola@users.noreply.github.com>
2022-08-24 17:43:01 +0200

    Update R_code_exam_Nigeria.r

--- R_code_exam_Nigeria.r
+++ R_code_exam_Nigeria.r
@@ -191,6 +191,18 @@ cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
 # Faccio un plot del DVI nel 2015 per visulizzare le condizioni della vegetazione 
 plot(dvi_2015, col=cl) + title(main="DVI 2015") # il giallo dovrebbe rapresentare il suolo nudo
 
+# DVI nel 2015
+dvi_2015 = n2015[[5]] - n2015[[4]]
+dvi_2015 # values : -531.666, 12364.91 (min, max)
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi_2015, col=cl) + title(main="DVI 2015")
+
+
+pdf("DVI_2015_plot.pdf")
+plot(dvi_2015, col=cl)
+title(main="DVI 2015")
+dev.off()
+
 ## CALCOLO DEL DVI NEL 2022 ##
 
 # DVI nel 2022
@@ -203,6 +215,18 @@ cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
 # PLOT
 # Faccio un plot del DVI nel 2022 per visualizzare le condizioni della vegetazione
 plot(dvi_2022, col=cl) + title(main="DVI 2022") # il giallo dovrebbe rapresentare il suolo nudo
+
+# DVI nel 2022
+dvi_2022 = n2022[[5]] - n2022[[4]]
+dvi_2022 # values : -2203.359, 13241.65 (min, max) 
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi_2022, col=cl) + title(main="DVI 2022")
+
+pdf("DVI_2022_plot.pdf")
+plot(dvi_2022, col=cl)
+title(main="DVI 2022")
+dev.off()
+
 
 # CONFRONTO TRA I DUE DVI 
 # confronto i due DVI creando un multiframe con 1 riga 2 colonne che contiene al suo interno le due immagini 
@@ -221,7 +245,18 @@ cld <- colorRampPalette(c("blue", "white", "red")) (100)
 # faccio un plot della differenza dei DVI delle due immagini per valutare ed evidenziare il cambiamento di stato di salute della foresta nel tempo trascorso. 
 plot(dvi_diff, col=cld) + title(main="DVI 2015 - DVI 2022") # le zone in rosso rappresentano la deforestazione o la scomparsa in generale di vegetazione nel arco di tempo analizato. 
 
-                                                     #### CALCOLO DEL NDVI (NORMAL DIFFERENCE VEGETATION INDEX) ####
+
+# Differenz dei DVI: 
+dvi_diff = dvi_2015 - dvi_2022 
+cld <- colorRampPalette(c("blue", "white", "red")) (100) 
+plot(dvi_diff, col=cld) + title(main="DVI 2015 - DVI 2022")
+
+pdf("differenza_DVI_plot.pdf")
+plot(dvi_diff, col=cld)
+title(main="DVI 2015 - DVI 2022")
+dev.off()
+
+                                                      #### CALCOLO DEL NDVI (NORMAL DIFFERENCE VEGETATION INDEX) ####
 
 # NDVI = (Riflettanza NIR - Riflettanza RED) / (Riflettanza NIR + Riflettanza RED)
 # Range NDVI (Imaggine a 16 bit) : -1 a 1
@@ -230,11 +265,24 @@ plot(dvi_diff, col=cld) + title(main="DVI 2015 - DVI 2022") # le zone in rosso r
 
 # NDVI nel 2015
 ndvi_2015 = (n2015[[5]] - n2015[[4]]) / (n2015[[5]] + n2015[[4]])
-
+ndvi_2015 # values : -0.02761471, 0.389767 (min, max) 
 # PLOT 
 # faccio il plot del NDVI nel 2015
 plot(ndvi_2015, col=cl) + title(main = "NDVI 2015")
-             
+
+
+# NDVI nel 2015
+ndvi_2015 = (n2015[[5]] - n2015[[4]]) / (n2015[[5]] + n2015[[4]])
+ndvi_2015 # values : -0.02761471, 0.389767 (min, max) 
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(ndvi_2015, col=cl) + title(main = "NDVI 2015")
+
+pdf("NDVI_2015_plot.pdf")
+plot(ndvi_2015, col=cl)
+title(main = "NDVI 2015")
+dev.off()
+
+
 ## CALCOLO NDVI NEL 2022
 
 # NDVI nel 2022
@@ -243,6 +291,16 @@ ndvi_2022 = (n2022[[5]] - n2022[[4]]) / (n2022[[5]] + n2022[[4]])
 # PLOT 
 # Faccio il plot del NDVI nel 2022
 plot(ndvi_2022, col=cl) + title(main = "NDVI 2022")
+
+ndvi_2022 = (n2022[[5]] - n2022[[4]]) / (n2022[[5]] + n2022[[4]])
+ndvi_2022 # values : -0.09720852, 0.4261148 (min, max) 
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(ndvi_2022, col=cl) + title(main = "NDVI 2022")
+
+pdf("NDVI_2022_plot.pdf")
+plot(ndvi_2022, col=cl)
+title(main = "NDVI 2022")
+dev.off()
 
 # CONFRONTO TRA I DUE NDVI 
 # confronto i due NDVI creando un multiframe con 1 riga e 2 colonne che contiene al suo interno le due immagini 
@@ -282,8 +340,18 @@ clc <- colorRampPalette(c("yellow", "red")) (100)
 # PLOT 
 # effetuo un plot dove si evidenzia la suddivisione nelle due classi che prendono i colori assegnati dalla colorRampPalette:
 plot(n2015_class$map, col=clc) + title(main ="suddivisione in classi 2015")
-# classe 1: aree di vegetazione (appare di colore giallo)
-# classe 2: aree di suolo nudo (appare di colore rosso) 
+# classe 1: aree di vegetazione (appare di colore rsso)
+# classe 2: aree di suolo nudo (appare di colore giallo) 
+
+# 2015 Suddivisioni classi 
+n2015_class <- unsuperClass(n2015, nClasses=2) 
+clc <- colorRampPalette(c("yellow", "red")) (100)
+plot(n2015_class$map, col=clc) + title(main ="suddivisione in classi 2015")
+
+pdf("n2015_class_plot.pdf")
+plot(n2015_class$map, col=clc)
+title(main ="suddivisione in classi 2015")
+dev.off()
 
 # SUDDIVISIONE IN 2 CLASSI DELL'IMMAGINE NEL 2022
 
@@ -296,9 +364,20 @@ clc <- colorRampPalette(c("yellow", "red")) (100)
 # PLOT
 # effetuo un plot dove si evidenzia la suddivisione nelle due classi che prendono i colori assegnati dalla colorRampPalette:
 plot(n2022_class$map, col=clc) + title(main ="suddivisione in classi 2022")
-# classe 1: aree di vegetazione (appare di colore giallo)
-# classe 2: aree di suolo nudo (appare di colore rosso) 
- 
+# classe 1: aree di vegetazione (appare di colore rosso)
+# classe 2: aree di suolo nudo (appare di colore giallo) 
+
+# 2022 suddivisione in classi 
+n2022_class <- unsuperClass(n2022, nClasses=2)
+clc <- colorRampPalette(c("yellow", "red")) (100)
+plot(n2022_class$map, col=clc) + title(main ="suddivisione in classi 2022")
+
+
+pdf("n2022_class_plot.pdf")
+plot(n2022_class$map, col=clc)
+title(main ="suddivisione in classi 2022")
+dev.off()
+
 # CONFRONTO LE DUE IMMAGINI CLASSIFICATE
 # confronto le due immagini classificate creando un multiframe con 1 riga e 2 colonne che contiene al suo interno le due immagini
 par(mfrow=c(1, 2))
@@ -315,33 +394,33 @@ plot(n2022_class$map, col=clc) + title(main ="suddivisione in classi 2022")
 # Calcolo le frequenze dell'immagine del 2015 classificata tramite la funzione freq: 
 freq(n2015_class$map)
 # value     count 
-# classe 1: 300520 Pixel (vegetazione)
-# classe 2: 105928 pixel (suolo nudo)
-#       NA: 180244 pixel (pixel bianchi dovti all'inclinazione dell'immagine satellitare non li considero nei calcoli)
+# classe 1: 303486 Pixel (vegetazione)
+# classe 2: 103030 pixel (suolo nudo)
+#       NA: 178660 pixel (pixel bianchi dovti all'inclinazione dell'immagine satellitare non li considero nei calcoli)
 
 # Calcolo il totale dei pixel dell'immagine del 2015 classificata per poi calcolarmi le percentuali di ciascuna classe:
-#  totale: 300520 + 105928 = 406448 pixel
-# quindi il totale dei pixel dell'immagine del 2015 classificata è 406448 pixel
+#  totale: 303486 + 103030 = 406516 pixel
+# quindi il totale dei pixel dell'immagine del 2015 classificata è 406516 pixel
 
 # PERCENTUALI DELL'IMMAGINE DEL 2015: 
 
 # Calcolo le percentuali dell'immagine del 2015 classificata per renderla confrontabile 
 # Calcolo tramite la semplice formula: count della classe * 100 / totale dei pixel della immagine classificata 
-tot2015 <- 406448 # assegno il totale dei pixel della immagine del 2015 classificata a tot2015
-perc_veg_2015 <- 300520 * 100 / tot2015    # 73.93812 %
-perc_soil_2015 <- 105928 * 100 / tot2015   # 26.06188 % 
+tot2015 <- 406516 # assegno il totale dei pixel della immagine del 2015 classificata a tot2015
+perc_veg_2015 <- 303486 * 100 / tot2015    # 74.65536 %
+perc_soil_2015 <- 103030 * 100 / tot2015   # 25.34464 % 
 
 # FREQUENZE DELL'IMMAGINE DEL 2022:
 
 # Calcolo le frequenze dell'immagine del 2022 classificata tramite la funzione freq: 
 freq(n2022_class$map)
 # value     count 
-# classe 1: 271822 Pixel (vegetazione)
-# classe 2: 136462 Pixel (suolo nudo)
+# classe 1: 269844 Pixel (vegetazione)
+# classe 2: 138440 Pixel (suolo nudo)
 #       NA: 176892 Pixel (Pixel bianchi dovuti all'inclinazione dell'immagine satellitare non li considero nei calcoli)
 
 # Calcolo il totale dei pixel dell'immagine del 2022 classificata per poi calcolarmi le percentuali di ciascuna classe:
-#  totale: 271822 + 136462 = 408284 pixel 
+#  totale: 269844 + 138440 = 408284 pixel 
 # quindi il totale dei pixel dell'immagine del 2022 classificata è 408284 pixel 
 
 # PERCENTUALI DELL'IMMAGINE DEL 2022: 
@@ -349,31 +428,50 @@ freq(n2022_class$map)
 # Calcolo le percentuali dell'immagine del 2022 classificata per renderla confrontabile 
 # Calcolo tramite la semplice formula: count della classe * 100 / totale dei pixel della immagine classificata 
 tot2022 <- 408284 # assegno il totale dei pixel della immagine del 2022 classificata a tot2022
-perc_veg_2022 <- 271822 * 100 / tot2022   # 66.5767 %
-perc_soil_2022 <- 136462 * 100 / tot2022  # 33.4233 %
+perc_veg_2022 <- 269844 * 100 / tot2022   # 66.09233 %
+perc_soil_2022 <- 138440 * 100 / tot2022  # 33.9077 %
 
 # CONFRONTO DEI DATI PERCENTUALI 
 
 #creo dataframe per confrontare i dati percentuali ottenuti dalle analisi precedenti 
 #creo prima le 3 colonne 
 class <- c("Vegetazione", "Suolo nudo") # assegno un vettore a un ogetto che rapresenta la prima colonna 
-percent_2015 <- c(73.93812, 26.06188) # assegno un vettore a un ogetto che rappresenta la seconda colonna 
-percent_2022 <- c(66.5767, 33.4233) # assegno un vettore a un ogetto che rappresenta la terza colonna  
+percent_2015 <- c(74.65536, 25.34464) # assegno un vettore a un ogetto che rappresenta la seconda colonna 
+percent_2022 <- c(66.09233, 33.9077) # assegno un vettore a un ogetto che rappresenta la terza colonna  
 
 # creo il dataframe tramite la funzione data.frame e la associo a multitemporal: 
 multitemporal <- data.frame(class, percent_2015, percent_2022)
 View(multitemporal) # Per visulizzare il dataframe in formato tabella
 
+
 #salvo il dataframe come file csv con la funzione write.csv lo apro con exel e lo salvo come immagine da mettere nella presentazione
-#write.csv(multitemporal, file = "multitemporal.csv") 
+# write.csv(multitemporal, file = "multitemporal.csv") 
 
 # CREAZIONE DI ISTOGRAMMI PER IL CONFRONTO DATI: 
 
 # Creo un istrgamma che rappresenti i dati percentuali ottenuti dalle due classi dell'immagine del 2015 classificata: 
-perc_2015 <- ggplot(multitemporal, aes(x=class, y=percent_2015, color=class)) + geom_bar(stat="identity", fill="white") + ggtitle("percentuale 2015")
+perc_2015 <- ggplot(multitemporal, aes(x=class, y=percent_2015, color=class)) + 
+             geom_bar(stat="identity", fill="white") + 
+             ggtitle("percentuale 2015")
+
+pdf("Percebtuale_2015_plot.pdf")
+ggplot(multitemporal, aes(x=class, y=percent_2015, color=class)) + geom_bar(stat="identity", fill="white") +  ggtitle("percentuale 2015")
+title(main = "percentuale 2015")
+dev.off()
 
 # Creo un istrgamma che rappresenti i dati percentuali ottenuti dalle due classi dell'immagine del 2022 classificata: 
-perc_2022 <- ggplot(multitemporal, aes(x=class, y=percent_2022, color=class)) + geom_bar(stat="identity", fill="white") + ggtitle("percentuale 2022")
+
+# Plot percentuale 2022
+perc_2022 <- ggplot(multitemporal, aes(x=class, y=percent_2022, color=class)) + 
+             geom_bar(stat="identity", fill="white") + 
+             ggtitle("percentuale 2022")
+
+pdf("multitemporal_dataframe.pdf")
+ ggplot(multitemporal, aes(x=class, y=percent_2022, color=class)) + 
+             geom_bar(stat="identity", fill="white") + 
+             ggtitle("percentuale 2022")
+title(main = "percentuale 2022")
+dev.off()
 
 # CONFRONTO DEI 2 ISCTOGRAMMI: 
 perc_2015 + perc_2022 # si nota come la percentuale delle aree a suolo nudo sia aumentata dal 2015 al 2022 e l'area vegetale sia diminuita 
@@ -430,11 +528,24 @@ g2 + g1
 # tramite l'analisi multivariata anziche scegliere una sola variabile posso compattare tutti i dati in un sistema più semplice 
 
 # ANALISI MULTIVARIATA SULLA IMMAGINE DEL 2015:
+
+#2015
 n2015_pca <- rasterPCA(n2015)
+summary(n2015_pca$model) 
 
 # faccio la summary del modello per vedere quanta variabilità spiega ogni componente 
 summary(n2015_pca$model) 
-# proprortion of Variance: #PC1 spiega il 76.69% #PC2 spiega il 15.90%  #PC3 spiega il 5.9% 
+# proprortion of Variance: #PC1 spiega il 76.70% #PC2 spiega il 15.89%  #PC3 spiega il 5.99% 
+
+# ogni percentuale la associo a uno oggetto per poi creare un dataframe:
+Pca_type <- c("Pc1", "Pc2", "Pc3")
+Percent_2015 <-c(76.70, 15.98, 5.99)
+
+# creo dataframe delle varie PCA:
+Pc_variability_2015 <- data.frame(Pca_type, Percent_2015)
+View(Pc_variability_2015)
+#salvo il dataframe come file csv con la funzione write.csv lo apro con exel e lo salvo come immagine da mettere nella presentazione
+# write.csv(Pc_variability_2015, file = "PCA_2015.csv")
 
 # PLOT
 # Effetto un plot con tutte le componenti 
@@ -447,8 +558,10 @@ pc3_2015 <- n2015_pca$map$PC3
 
 # tramite ggplot faccio il plot delle singole componenti
 # associo al plot un oggetto 
-# PC1 
-gpc1_2015 <- ggplot() + geom_raster(pc1_2015, mapping=aes(x=x, y=y, fill=PC1)) + ggtitle("PC1 2015")
+# PC1 2015
+gpc1_2015 <- ggplot() + 
+             geom_raster(pc1_2015, mapping=aes(x=x, y=y, fill=PC1)) + 
+             ggtitle("PC1 2015")
 #PC2 
 gpc2_2015 <- ggplot() + geom_raster(pc2_2015, mapping=aes(x=x, y=y, fill=PC2)) + ggtitle("PC2 2015")
 #PC3 
@@ -462,9 +575,12 @@ gpc1_2015 + gpc2_2015 + gpc3_2015
 # calcolo la deviazione standard della PC1 del 2015 sempre con una moving window 3 x 3 
 sd_pc1_2015 <- focal(pc1_2015, matrix(1/9, 3, 3), fun=sd)
 
-# PLOT
+# PLOT 
 # faccio ggplot della deviazione standard della pc1 usando viridis 
-Im_2015 <- ggplot() + geom_raster(sd_pc1_2015, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option = "cividis") + ggtitle("deviazione standard della PC1 del 2015 tramite il pachetto viridis")
+Im_2015 <- ggplot() + 
+           geom_raster(sd_pc1_2015, mapping=aes(x=x, y=y, fill=layer)) + 
+           scale_fill_viridis(option = "cividis") + 
+           ggtitle("deviazione standard della PC1 del 2015 tramite il pachetto viridis")
 # bassa variabilità dove si ha la foresta e il suolo nudo
 # la massima variabilità è in corrispondenza delle zone di transizione tra vegetazione e suolo nudo 
 # si riesce a notare anche le strade che tagliano la vegetazione per collegare le varie città 
@@ -479,6 +595,17 @@ n2022_pca <- rasterPCA(n2022)
 # faccio la summary del modello per vedere quanta variabilità spiega ogni componente 
 summary(n2022_pca$model) 
 # proprortion of Variance: #PC1 spiega il 79.86% #PC2 spiega il 14.03%  #PC3 spiega il 4.09% 
+
+# ogni percentuale la associo a uno oggetto per poi creare un dataframe:
+Pca_type <- c("Pc1", "Pc2", "Pc3")
+Percent_2022 <-c(79.86, 79.86, 4.09)
+
+# creo dataframe delle varie PCA:
+Pc_variability_2022 <- data.frame(Pca_type, Percent_2022)
+View(Pc_variability_2022)
+#salvo il dataframe come file csv con la funzione write.csv lo apro con exel e lo salvo come immagine da mettere nella presentazione
+# write.csv(Pc_variability_2022, file = "PCA_2022.csv")
+
 # faccio un plot con tutte le componenti 
 plot(n2022_pca$map) 
 
@@ -488,8 +615,12 @@ pc2_2022 <- n2022_pca$map$PC2
 pc3_2022 <- n2022_pca$map$PC3
 
 #tramite ggplot faccio il plot delle singole componenti, associo al plot un oggetto 
-#PC1 
-gpc1_2022 <- ggplot() + geom_raster(pc1_2022, mapping=aes(x=x, y=y, fill=PC1)) + ggtitle("PC1 2022")
+
+# PC1 2022
+gpc1_2022 <- ggplot() + 
+             geom_raster(pc1_2022, mapping=aes(x=x, y=y, fill=PC1)) + 
+             ggtitle("PC1 2022")
+
 #PC2 
 gpc2_2022 <- ggplot() + geom_raster(pc2_2022, mapping=aes(x=x, y=y, fill=PC2)) + ggtitle("PC2 2022")
 #PC3 
@@ -500,12 +631,18 @@ gpc1_2022 + gpc2_2022 + gpc3_2022
 
 # per vedere la variabilità calcolo la deviazione standard sulla PC1 di entrambe le immagini 
 # calcolo la deviazione standard della PC1 del 2015 sempre con una moving window 3 x 3 
+
+# CALCOLO DEVIAZIONE STANDARD DEL 2022
 sd_pc1_2022 <- focal(pc1_2022, matrix(1/9, 3, 3), fun=sd)
 
+# PLOT 2022
 # faccio ggplot della deviazione standard della pc1 usando viridis 
-Im_2022 <- ggplot() + geom_raster(sd_pc1_2022, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option = "cividis") + ggtitle("deviazione standard della PC1 del 2022 tramite il pachetto viridis")
-#bassa variabilità dove si ha la foresta 
-#si hanno dei piccoli picchi di massima varibilità in corrispondenza delle zone di suolo nudo 
+Im_2022 <- ggplot() + 
+           geom_raster(sd_pc1_2022, mapping=aes(x=x, y=y, fill=layer)) + 
+           scale_fill_viridis(option = "cividis") + 
+           ggtitle("deviazione standard della PC1 del 2022 tramite il pachetto viridis") 
+# bassa variabilità dove si ha la foresta 
+# si hanno dei piccoli picchi di massima varibilità in corrispondenza delle zone di suolo nudo 
 
 #visulizziamo assieme i due plot: ggplot dell'immagine del 2015 e la deviazione standard di PC1 basata su mw 3 x 3
 g1_2022 + Im_2022
